8db573bd690ba8f679c45a0465150dea645ce30a
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-02 17:40:10 +0100

    Create admin user in initalizer.

--- app/initializer.py
+++ app/initializer.py
@@ -1,9 +1,9 @@
 from app.models import User, db, Room
 from flask_bcrypt import Bcrypt
 
-def create_default_user(bcrypt: Bcrypt):
 
-    # Verifica che l'utente amministratore non esista
+def create_default_user(bcrypt: Bcrypt):
+    # Verifica che l'utente "mario.rossi@gmail.com" non esista
     user = User.query.filter_by(email="mario.rossi@gmail.com").first()
     if not user:
         hashed_password = bcrypt.generate_password_hash("Rossi2025").decode('utf-8')
@@ -14,6 +14,22 @@ def create_default_user(bcrypt: Bcrypt):
         db.session.add(admin_user)
         db.session.commit()
         print("User created with email 'mario.rossi@gmail.com' and password 'Rossi2025'")
+
+
+def create_admin_user(bcrypt: Bcrypt):
+    # Verifica che l'utente amministratore non esista
+    user = User.query.filter_by(email="admin@gmail.com").first()
+    if not user:
+        hashed_password = bcrypt.generate_password_hash("Admin2025").decode('utf-8')
+        admin_user = User(
+            email="admin@gmail.com",
+            password_hash=hashed_password,
+            isAdmin=True
+        )
+        db.session.add(admin_user)
+        db.session.commit()
+        print("User created with email 'admin@gmail.com' and password 'Admin2025'")
+
 
 def initialize_rooms():
     predefined_rooms = [
