75442926b5c4c408f2bae742aea5fb46ec8eb29b
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-02 15:40:05 +0100

    Added app JWT configuration in app __init__.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import create_app, db
 
-app = create_app()
+app = create_app()  # Crea un'istanza dell'app Flask
 
-with app.app_context():
-    db.create_all()
+with app.app_context():  # Crea il contesto dell'app, per effetuare operazioni legate al database
+    db.create_all()  # Crea le tabelle nel database in base ai modelli(models) definiti.
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Avvia il server Flask in modalità di debug,
