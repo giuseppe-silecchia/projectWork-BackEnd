2928bc9e4c98bf210e5bcce399d3b0221cc8925a
Giuseppe Silecchia <giuseppe.silecchia@masvis.com>
2025-01-02 15:41:54 +0100

    Addeed User model in models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,25 @@
 from . import db
 from datetime import datetime, timezone
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
 
 
 class Room(db.Model):
